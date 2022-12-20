b4cf2663a81a2fbb28f3ee8c3800a4b27691a122
web-Matheus <matheussantosjere02@gmail.com>
2022-12-20 09:05:23 -0300

    err ajustes

--- src/components/main-page/main.js
+++ src/components/main-page/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios from  'axios';
+// import axios from  'axios';
 import './main.css'
 const Main = () => {
   // const [data, setData] = useState([]);
@@ -11,15 +11,15 @@ const Main = () => {
   // })
   return (
     <div class="columns-2 md:columns-3 lg:columns-4">
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/1" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/2" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/3" alt='image aleatória vindo de uma api'  />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/4" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/5" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/6" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/7" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/8" alt='image aleatória vindo de uma api' />
-      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/9" alt='image aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/1" alt='foto aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/2" alt='ft aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/3" alt='ft aleatória vindo de uma api'  />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/4" alt='ft aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/5" alt='ft aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/6" alt='ft aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/7" alt='ft aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/8" alt='ft aleatória vindo de uma api' />
+      <img class="mb-4 rounded-lg" src="https://source.unsplash.com/random/9" alt='ft aleatória vindo de uma api' />
       ....
       ....
     </div>
